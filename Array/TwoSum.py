61e63a0a9b8bd04bc06498ed20acd70bb2cf0726
EduardoLSS <37881050+EduardoLSS@users.noreply.github.com>
2025-11-17 15:39:19 -0300

    List problem checking correction

--- Array/TwoSum.py
+++ Array/TwoSum.py
@@ -1,3 +1,4 @@
+from typing import List # Not used in Leetcode, imported to stop problem check in VSCode
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         neededValue = {}
